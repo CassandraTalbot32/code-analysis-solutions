1ca62f9d7c85be71ca48cde214faa389e329d553
Cassandra Talbot <40823509+CassandraTalbot32@users.noreply.github.com>
2022-02-21 00:48:20 +0000

    Update binksol4.py

--- binksol4.py
+++ binksol4.py
@@ -1,20 +1,23 @@
 import csv
 import pprint
-from datetime import datetime as dt
-
-from pandas.conftest import datetime64_dtype
+from datetime import datetime
 
 
-dict = {}
+dates = {}
 
 with open('bink_data.csv', 'r') as f:
     for row in csv.DictReader(f):
-        date_col1 = datetime.strptime(row[7], '01-06-2009')
+        if datetime.date('01-06-2009') < datetime.date('31-08-2007'):
+            dates.append()
+            print(dates)
 
-        date_col2 = datetime.strptime(row[9], '31-08-2007')
+        #if col1 is > 
+        #date_col1 = datetime.strptime(row[7], '01-06-2009')
+
+        #date_col2 = datetime.strptime(row[9], '31-08-2007')
         
-        if datetime64_dtype in range date_col1 and date_col2:
-            dict.append()
-            print{dict}
+        #if datetime.date in range date_col1 and date_col2:
+         #   dict.append()
+          #  print{dict}
         #if  dt <= int(line['Lease Start Date']) <= 25:
          #   pprint.pp(line)
