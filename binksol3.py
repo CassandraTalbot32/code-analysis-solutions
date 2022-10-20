f38e770c2131f50ba9916b72ba77b37e8bf8c4cf
Cassandra Talbot <40823509+CassandraTalbot32@users.noreply.github.com>
2022-10-20 09:22:36 +0100

    Update binksol3.py

--- binksol3.py
+++ binksol3.py
@@ -3,17 +3,17 @@ import pprint
 import unittest
 
 
-#3.create dictionary containing tenant name and count of masts for each tenant, output to console in readable form
-#tenant name : count of masts
-#how many times does tenant name appear in record
-#create empty dict
+# 3.create dictionary containing tenant name and count of masts for each tenant, output to console in readable form
+# tenant name : count of masts
+# how many times does tenant name appear in record
+# create empty dict
 name_count = {}
-#open file parse data
+# open file parse data
 with open('bink_data.csv', 'r') as count:
     csvReader = csv.reader(count, delimiter=",", quotechar='"')
 
     next(csvReader)
-#point to column 6 and request number of instances for each name
+# point to column 6 and request number of instances for each name
     for row in csvReader:
         if row [6] not in name_count:
             name_count[row[6]] = 1 
